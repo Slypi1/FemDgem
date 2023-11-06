e0a75b84ccb13e34390a0196099b34e9ac6f280c
Klim Shanov <k.shanov@maxfungames.com>
2023-11-06 23:02:50 +0700

    Пездец

--- Assets/Script/Mihanik/File.cs
+++ Assets/Script/Mihanik/File.cs
@@ -2,19 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 
 public class File : MonoBehaviour
 {
+    [SerializeField] private List<ProfileSettings> profiles;
     [SerializeField] private TMP_Text _name;
 
     public static Action<string> OnNameHiro;
 
+    private int _current;
 
     public void OnQuestioning()
     {
-        OnNameHiro(_name.text);
+        var name = profiles[_current].Nickname;
+        OnNameHiro(name);
         gameObject.SetActive(false);
+    }
+
+    public void NextFile()
+    {
+        _current++;
+        if (_current >= profiles.Count)
+            _current = 0;
     }
 }
