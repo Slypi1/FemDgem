e4a510b60fb2f15f9d2f4a91959bfca2c7c9d03a
Slypi <anastasuasluckix94@gmail.com>
2023-11-06 23:19:24 +0700

    Комит

--- Assets/Script/Mihanik/Arrow.cs
+++ Assets/Script/Mihanik/Arrow.cs
@@ -12,6 +12,9 @@ public class Arrow : MonoBehaviour,IBeginDragHandler,IDragHandler
     [SerializeField] private Image _smallArrow;
     public Vector3 mousePos, position;
     private Vector2 mousePosition;
+    [SerializeField] private Image _officeBoss;
+    [SerializeField] private Image _startoffic;
+    public static Action _isWath;
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -20,6 +23,11 @@ public class Arrow : MonoBehaviour,IBeginDragHandler,IDragHandler
         var angle = Mathf.Atan2(mouseCoord.x,mouseCoord.y) * Mathf.Rad2Deg;
          Debug.Log(angle);
         _smallArrow.transform.Rotate(new Vector3(0, 0,  -angle), 2.5f);
+        if (angle > -60f)
+        {
+            _officeBoss.gameObject.SetActive(true);
+            _isWath();
+        }
        /* Vector3 relative = transform.InverseTransformPoint(eventData.position);
         float angle = Mathf.Atan2(relative.x, relative.y) * Mathf.Rad2Deg;
         if (angle >= 45f)
